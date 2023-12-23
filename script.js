5f5daff457e44a6322c3b9467c86431256c373b2
Prakash Kumar <141754712+prakash-ydv@users.noreply.github.com>
2023-12-23 12:57:18 +0530

    script for analog clock and some code optimization

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+alert("Made with ❣️ by Prakash");
+
+hr_hand= document.querySelector('#hr_hand');
+mn_hand= document.querySelector('#mn_hand');
+sn_hand= document.querySelector('#sn_hand');
+
 setInterval(() => {
     d = new Date();
     htime = d.getHours();
@@ -6,25 +12,16 @@ setInterval(() => {
     document.getElementById("hour").innerHTML = htime + ":";
     document.getElementById("minute").innerHTML = mtime + ":";
     document.getElementById("second").innerHTML = stime;
+
+    sn_rotate=6*stime;
+    mn_rotate=6*mtime;
+    hr_rotate=(30*htime)+(mtime*0.5);
+
+    hr_hand.style.rotate=(`${hr_rotate}deg`);
+    mn_hand.style.rotate=(`${mn_rotate}deg`);
+    sn_hand.style.rotate=(`${sn_rotate}deg`);
 }, 1000);
 
-setInterval(() => {
-    d = new Date();
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
-
-    if (htime > 12) {
-        new_htime = htime - 12;
-        document.getElementById("hour2").innerHTML = new_htime + ":";
-    }else{
-        document.getElementById("hour2").innerHTML = htime + ":";
-    }
-
-    
-    document.getElementById("minute2").innerHTML = mtime + ":";
-    document.getElementById("second2").innerHTML = stime;
-}, 1000);
 
 
 
